55bac5af171a5fc6bd107312140987761ea29616
chiragpro21 <137913787+chiragpro21@users.noreply.github.com>
2023-08-01 21:14:20 +0530

    Create playjunction.is-an.app.js

---
+++ domains/playjunction.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Website'
+  domain: 'is-an.app',
+  subdomain: 'playjunction',
+  owner: {
+    repo: 'random-pass-gen.ytbin.repl.co',
+    email: 'fauzi.ardiantama@gmail.com',
+  },
+  record: {
+    CNAME: 'b995a6d2-bb15-4a44-bdcb-7bf2916394c3.id.repl.co',
+  },
+  proxy: false,
+})
